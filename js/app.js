949481663b9b22bfca365901ed93f71f77487462
Leandro Souza <souza.leandro78@gmail.com>
2025-01-22 14:14:27 -0300

    Adicionando validação de repetição de produto.
    Caso o produto já tenha sido adicionado ao carrinho anteriormente, o programa irá "empilhar" os dois.
    
    Antes o produto apareceria duplicado no carrinho.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+console.log('Executando v2.');
+let listaCarrinho = document.getElementById('lista-produtos');
 limpar();
 
 function adicionar() {
@@ -6,24 +8,29 @@ function adicionar() {
     //Recuperar a quantidade
     let campoQtd = document.getElementById('quantidade');
     let quantidade = parseInt(campoQtd.value);
-    console.log(`Quantidade: ${quantidade}`); //OK
+    //console.log(`Quantidade: ${quantidade}`); //OK
 
     
-    if (quantidade == '' || quantidade <= 0 || Number.isFinite(quantidade) == false) { //quantidade precisa ser maior zero e não pode ser vazia
+    if (quantidade == '' || quantidade <= 0 || Number.isFinite(quantidade) == false){ //quantidade precisa ser maior zero e não pode ser vazia
         alert('Quantidade precisa ser um valor numérico inteiro maior que 0.');
         return
     }
 
     //Recuperar o produto
     let produtoEscolhido = document.getElementById('produto'); //retorna uma string com o nome do produto e o valor
-    console.log(`Produto escolhido: ${produtoEscolhido.value}`); //OK
+    //console.log(`Produto escolhido: ${produtoEscolhido.value}`); //OK
 
     //Separar o nome do produto e o valor
-    let nomeProduto = produtoEscolhido.value.split(' - ')[0];
-    let valorProduto = parseFloat(produtoEscolhido.value.split(' - R$')[1]) * quantidade;
-    console.log(`Nome do produto ${nomeProduto}, valor total: R$ ${valorProduto}`); //OK
 
-    criarNovaSecao(quantidade, nomeProduto, valorProduto); //cria a nova seção que será exibida no carrinho
+    let nomeProduto = produtoEscolhido.value.split(' - ')[0];
+    let valorProduto = parseInt(produtoEscolhido.value.split(' - R$')[1]) * quantidade;
+    //console.log(`Nome do produto ${nomeProduto}, valor total: R$ ${valorProduto}`); //OK
+
+    if (verificarRepeticao(nomeProduto) > 0) {
+        atualizarExistente(verificarRepeticao(nomeProduto),quantidade,valorProduto);
+    } else {
+        criarNovaSecao(quantidade, nomeProduto, valorProduto);
+    }
 
     //Agora precisa limpar a quantidade
     campoQtd.value = '';
@@ -31,15 +38,13 @@ function adicionar() {
     //O próximo passo é recuperar o total anterior.
     let totalAnterior = document.getElementById('valor-total'); //recupera todo o elemento deste ID
     let valorTotalAnterior = totalAnterior.textContent.replace('R$',''); //remove o símbolo 'R$' da string
-    valorTotalAnterior = parseFloat(valorTotalAnterior); //converte a string em número
+    valorTotalAnterior = parseInt(valorTotalAnterior); //converte a string em número
     let valorTotalAtualizado = valorTotalAnterior + valorProduto;
     totalAnterior.textContent = `R$${valorTotalAtualizado}`;
 }
 
 function criarNovaSecao(quantidade, nomeProduto, valorProduto) {
     
-    //recuperar lista de produtos no carrinho {id = lista-produtos}
-    let carrinho = document.getElementById('lista-produtos');
     let novaSecao = document.createElement('section'); //nova secão criada, mas ainda não "pendurada" no carrinho
 
     novaSecao.classList.add('carrinho__produtos__produto'); //classe inserida
@@ -61,13 +66,11 @@ function criarNovaSecao(quantidade, nomeProduto, valorProduto) {
     novaSecao.appendChild(novoItemValor);
 
     //"pendurar" a seção no carrinho
-    carrinho.appendChild(novaSecao);
+    listaCarrinho.appendChild(novaSecao);
 
 }
 
 function limpar() {
-    let listaCarrinho = document.getElementById('lista-produtos');
-    
     while (listaCarrinho.firstChild) { //Conforme explicado pela Luri: "Em JavaScript, qualquer valor que não seja null, undefined, 0, false, NaN ou uma string vazia é considerado verdadeiro."
         listaCarrinho.removeChild(listaCarrinho.firstChild); //remove o primeiro filho
     } //repete enquanto houver algum filho
@@ -75,6 +78,31 @@ function limpar() {
     document.getElementById('valor-total').textContent = 'R$0';
 }
 
-function verificarRepeticao() {
-    
+function verificarRepeticao(nomeProduto) {
+    //console.log('Verificando repetição.')
+    let listaFilhosCarrinho = listaCarrinho.querySelectorAll('span');
+
+    for (i = 1; i < listaFilhosCarrinho.length; i += 3) {
+        //console.log(`"${listaFilhosCarrinho[i].textContent}" sendo analisado.`);
+        if (listaFilhosCarrinho[i].textContent.includes(nomeProduto)) {
+            //console.log('Produto repetido encontrado.');
+            return i;
+        }
+    }
+    //console.log('Produto não repetido.');
+    return false;
+}
+
+function atualizarExistente(i, quantidade, valorProduto) {
+    //console.log('Entrando na atualização de produto existente.')
+    let listaFilhosCarrinho = listaCarrinho.querySelectorAll('span'); //achei interessante sempre gerar a lista para atualizar a array
+
+    let novaQuantidade = quantidade + parseInt(listaFilhosCarrinho[i - 1].textContent.split('x'));
+    //console.log(listaFilhosCarrinho[i + 1].textContent);
+    //console.log(listaFilhosCarrinho[i + 1].textContent.split('R$')); //retorna um array onde a posição 1 é o valor procurado.
+    //console.log(parseInt(listaFilhosCarrinho[i + 1].textContent.split('R$')));
+    let novoValorProduto = valorProduto + parseInt(listaFilhosCarrinho[i + 1].textContent.split('R$')[1]);
+    //console.log(novoValorProduto);
+    listaFilhosCarrinho[i - 1].innerHTML = `${novaQuantidade}x`;
+    listaFilhosCarrinho[i + 1].innerHTML = `R$${novoValorProduto}`;
 }
